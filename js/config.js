d85414958ed32ef3d03f1bc5c2c20123ad95dbc5
Empty <denissevrugin4@gmail.com>
2026-09-21 00:23:12 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,23 @@
+/* ============================================================
+   CONFIG: инициализация Telegram + константы
+   ============================================================ */
+
+// Telegram WebApp
+const tg = window.Telegram.WebApp;
+tg.expand();
+tg.requestFullscreen();
+
+// Ссылка на продавца
+const SELLER_LINK = 'https://t.me/Dinis_xru_xru';
+
+// Stars
+const STARS_RATE = 1.39;
+const STARS_MIN = 50;
+const STARS_MAX = 1500;
+const STARS_STEP = 50;
+
+// Robux
+const ROBUX_RATE = 1.25;
+const ROBUX_MIN = 50;
+const ROBUX_MAX = 1500;
+const ROBUX_STEP = 50;
