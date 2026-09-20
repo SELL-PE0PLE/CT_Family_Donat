5106a22fc154067166c5b9a1f0c296f26d90bd34
Empty <denissevrugin4@gmail.com>
2026-09-21 01:20:51 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,22 +1,18 @@
 /* ============================================================
-   CONFIG: инициализация Telegram + константы
+   CONFIG
    ============================================================ */
 
-// Telegram WebApp
 const tg = window.Telegram.WebApp;
 tg.expand();
 tg.requestFullscreen();
 
-// Ссылка на продавца
 const SELLER_LINK = 'https://t.me/Dinis_xru_xru';
 
-// Stars
 const STARS_RATE = 1.39;
 const STARS_MIN = 50;
 const STARS_MAX = 1500;
 const STARS_STEP = 50;
 
-// Robux
 const ROBUX_RATE = 1.25;
 const ROBUX_MIN = 50;
 const ROBUX_MAX = 1500;
